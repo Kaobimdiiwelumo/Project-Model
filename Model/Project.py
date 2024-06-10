7af5209167f168052c7b4683299846fa72488093
kaobimdi iwelumo <kaobimdi.iwelumo@pau.edu.ng>
2024-06-10 18:03:54 +0100

    Fix script execution issue

--- Model/Project.py
+++ Model/Project.py
@@ -14,10 +14,10 @@ CORS(app)  # Enable CORS for all routes
 # logging.basicConfig(filename='api.log', level=logging.INFO)
 
 # Load the ML model using pickle
-with open('/Users/HP1030G2/Documents/FINAL YEAR PROJECT/Project-Model/Model/model.pkl', 'rb') as model_file:
+with open('Model/model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-with open('/Users/HP1030G2/Documents/FINAL YEAR PROJECT/Project-Model/Model/modelcolumns.pkl', 'rb') as columns_file:
+with open('Model/modelcolumns.pkl', 'rb') as columns_file:
     columns_used = pickle.load(columns_file)
 
     print(columns_used)
